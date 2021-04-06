1f9e479072170c8751ef93bd7c03aa007791fb7e
Boondock19 <josematiasgonzalez20@gmail.com>
2021-04-06 16:05:22 -0400

    creando operation_handler.js

--- js/caculadora.js
+++ js/caculadora.js
@@ -1,30 +1,34 @@
+import {operationHandler} from "js/operation_handler.js"
 document.addEventListener("DOMContentLoaded", function () {
   //We need to get the display part of the calculator
   display = document.querySelector(".calculadora-display");
-  console.log("Este es el display",display.innerHTML)
+  console.log("Este es el display", display.innerHTML);
   //Select the decimal button and function
-  decimal=document.querySelector(".decimal")
-  decimal.addEventListener("click",function(){
-      if (display.innerHTML.contains(".")){
-          
-      }
-  })
+  decimal = document.querySelector(".decimal");
+  decimal.addEventListener("click", function () {
+    if (display.innerHTML.includes(".")) {
+      display.innerHTML = display.innerHTML;
+    } else {
+      displayCalculator = displayCalculator + ".";
+      display.innerHTML = displayCalculator;
+    }
+  });
   // WE get all the number buttons
   number = document.querySelectorAll(".number");
   console.log("Cargo el scritp");
   console.log(number);
- //Creation of a var to hold the values/numbers we enter in the calculator
-  var displayCalculator=display.innerHTML
+  //Creation of a var to hold the values/numbers we enter in the calculator
+  var displayCalculator = display.innerHTML;
   number.forEach((element) => {
     element.addEventListener("click", function () {
-        if ((display.innerHTML == "0")) {
-          console.log("Esta entrando en el primer if")
-          displayCalculator = element.innerHTML;
-          display.innerHTML= displayCalculator
+      if (display.innerHTML == "0") {
+       //console.log("Esta entrando en el primer if");
+        displayCalculator = element.innerHTML;
+        display.innerHTML = displayCalculator;
       } else {
-        console.log("Esta entrando en el segundo if")
-        displayCalculator=displayCalculator+element.innerHTML
-        display.innerHTML= displayCalculator;
+        //console.log("Esta entrando en el segundo if");
+        displayCalculator = displayCalculator + element.innerHTML;
+        display.innerHTML = displayCalculator;
       }
       console.log(element.innerHTML);
     });
---
+++ js/operation_handler.js
@@ -0,0 +1,26 @@
+
+
+
+operationHandler((firstNumber, secondNumber, operation) => {
+  if (operation == "+") {
+    console.log(`Esta es la operacion ${operation} entre
+         ${firstNumber} y el segundo numero  ${secondNumber}`);
+  } else if (operation == "-") {
+    console.log(`Esta es la operacion ${operation} entre
+         ${firstNumber} y el segundo numero  ${secondNumber}`);
+  } else if (operation == "x") {
+    console.log(`Esta es la operacion ${operation} entre
+         ${firstNumber} y el segundo numero  ${secondNumber}`);
+  } else if (operation == "/") {
+    console.log(`Esta es la operacion ${operation} entre
+         ${firstNumber} y el segundo numero  ${secondNumber}`);
+  } else if (operation == "%") {
+    console.log(`Esta es la operacion ${operation} entre
+         ${firstNumber} y el segundo numero  ${secondNumber}`);
+  } else if (operation == "mod") {
+    console.log(`Esta es la operacion ${operation} entre
+         ${firstNumber} y el segundo numero  ${secondNumber}`);
+  }
+});
+
+export { operationHandler };
