ece08866c440e13e5cc758f032ffd67b21eae4c2
Boondock19 <josematiasgonzalez20@gmail.com>
2021-04-05 21:30:21 -0400

    segundo commit

--- caculadora.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener("DOMContendLoaded",function (){
-    number=document.querySelectorAll("number");
-    console.log("Cargo el scritp")
-    console.log(number)
-})
---
+++ js/caculadora.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", function () {
+  //We need to get the display part of the calculator
+  display = document.querySelector(".calculadora-display");
+  console.log("Este es el display",display.innerHTML)
+  //Select the decimal button and function
+  decimal=document.querySelector(".decimal")
+  decimal.addEventListener("click",function(){
+      if (display.innerHTML.contains(".")){
+          
+      }
+  })
+  // WE get all the number buttons
+  number = document.querySelectorAll(".number");
+  console.log("Cargo el scritp");
+  console.log(number);
+ //Creation of a var to hold the values/numbers we enter in the calculator
+  var displayCalculator=display.innerHTML
+  number.forEach((element) => {
+    element.addEventListener("click", function () {
+        if ((display.innerHTML == "0")) {
+          console.log("Esta entrando en el primer if")
+          displayCalculator = element.innerHTML;
+          display.innerHTML= displayCalculator
+      } else {
+        console.log("Esta entrando en el segundo if")
+        displayCalculator=displayCalculator+element.innerHTML
+        display.innerHTML= displayCalculator;
+      }
+      console.log(element.innerHTML);
+    });
+  });
+});
